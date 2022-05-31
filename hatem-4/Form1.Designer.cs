d1fd69ee1b7462a96cfaadacd97997323b9b2217
ahm4d98 <81179645+ahm4d98@users.noreply.github.com>
2022-05-31 12:30:19 +0200

    Add files via upload

---
+++ hatem-4/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿
+namespace hatem4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.zv = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(85, 125);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(85, 79);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 27);
+            this.textBox3.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(61, 403);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(730, 227);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(344, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 61);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Add new cell";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_3);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(864, 403);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button2.Size = new System.Drawing.Size(133, 75);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "DONE";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(367, 139);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(198, 48);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "add new atom";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-18, -83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-39, -151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Y";
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button4.Location = new System.Drawing.Point(-15, 125);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Z";
+            this.button4.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(662, 53);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(161, 29);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "start";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(662, 139);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(161, 29);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "stop";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(17, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 28);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "xv";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(17, 271);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 28);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "yv";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // zv
+            // 
+            this.zv.AutoSize = true;
+            this.zv.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.zv.Location = new System.Drawing.Point(12, 198);
+            this.zv.Name = "zv";
+            this.zv.Size = new System.Drawing.Size(25, 21);
+            this.zv.TabIndex = 18;
+            this.zv.Text = "zv";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(85, 221);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(125, 27);
+            this.textBox4.TabIndex = 18;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(85, 271);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(125, 27);
+            this.textBox5.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(18, 324);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 28);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "zv";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(85, 324);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(125, 27);
+            this.textBox6.TabIndex = 21;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(85, 179);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(125, 27);
+            this.textBox7.TabIndex = 22;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(18, 179);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(24, 28);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "V";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(85, 370);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(125, 27);
+            this.textBox8.TabIndex = 24;
+            // 
+            // button8
+            // 
+            this.button8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button8.Location = new System.Drawing.Point(2, 368);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(58, 29);
+            this.button8.TabIndex = 26;
+            this.button8.Text = "R";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1139, 680);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "atom simulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label zv;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ hatem-4/Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+namespace hatem4
+{
+
+    public partial class Form1 : Form
+    {
+        int number;
+
+        cell root = null;
+        cell currentc = null;
+        Timer timer = new Timer();
+        bool is_On = false;
+        int n;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+
+
+
+        private void insertcell(ref cell list, cell a)
+        {
+            if (list == null)
+            { list = a; }
+            else
+            {
+                cell cur = list;
+                while ((cur.getNext() != null))
+                {
+                    cur.setNext(a);
+                    cur = cur.getNext();
+
+
+                }
+            }
+        }
+
+        private void updateVelocity(object sender, EventArgs e)
+        {
+            cell cellPointer = root;
+            Atom atomPointer;
+
+            Random rand = new Random();
+            double change;
+            int range = 2 - (-1);
+
+            while (cellPointer != null)
+            {
+                atomPointer = cellPointer.getAtom();
+                while (atomPointer != null)
+                {
+                    //  rand * range + min
+                    change = rand.NextDouble() * range + (-1);
+                    double newVelocity = atomPointer.get_v() + change;
+                    atomPointer.set_v(newVelocity);
+                    double newVX = atomPointer.get_vx() + change;
+                    atomPointer.set_vx(newVX);
+                    double newVY = atomPointer.get_vy() + change;
+                    atomPointer.set_vy(newVY);
+                    double newVZ = atomPointer.get_vz() + change;
+                    atomPointer.set_vz(newVZ);
+                    //update x,y,z
+                    double newx = atomPointer.get_x() + atomPointer.get_vx() ;
+                    atomPointer.set_x(newx);
+                    double newy = atomPointer.get_x() + atomPointer.get_vy() ;
+                    atomPointer.set_y(newy);
+                    double newz = atomPointer.get_x() + atomPointer.get_vz() ;
+                    atomPointer.set_z(newz);
+
+
+                    atomPointer = atomPointer.getNext();
+                }
+                cellPointer = cellPointer.getNext();
+            }
+            display();
+        }
+         void distance(Atom atom1, Atom atom2)
+        {
+            listView1.Items.Add("calculat distance between atoms");
+            Atom a1 = atom1;
+            Atom a2 = atom2;
+            while (a1 != null)
+            {
+                a2 = a1.getNext();
+                while (a2 != null)
+                {
+
+                    double dist = Math.Sqrt(Math.Pow(a1.get_x() - a2.get_x(), 2) + Math.Pow(a1.get_y() - a2.get_y(), 2) + Math.Pow(a1.get_z() - a2.get_z(), 2));
+                    dist -= a1.get_r() + a2.get_r();
+                    if (dist < a1.get_r() + a2.get_r())
+                    {
+                        listView1.Items.Add("Atom " + a1.get_n() + " make collision with" + " Atom " + a2.get_n());
+                    }
+                    a2 = a2.next;
+                }
+                a1 = a1.next;
+            }
+        }
+
+
+        private void display()
+        {
+            cell cel = root;
+            Atom att;
+            while (cel != null)
+            {
+                att = cel.getAtom();
+                listView1.Items.Add("cell " + cel.getNum());
+
+                while(att != null)
+                {
+
+                    listView1.Items.Add(" x = " + att.get_x() + ", y = " + att.get_y() +", z = " + att.get_z() + ", v = " + att.get_v().ToString("n2"));
+                    att = att.getNext();
+
+
+
+                }
+                distance(cel.getAtom(), cel.getAtom());
+
+                cel = cel.getNext();
+            }
+
+
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click_3(object sender, EventArgs e)
+        {
+            cell newcell = new cell(number);
+            currentc = newcell;
+            number++;
+            insertcell(ref root, newcell);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            display();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            Atom newAtom;
+            double x, y, z , v;
+            double xv,yv,zv;
+            double r;
+            x = Convert.ToDouble(textBox1.Text);
+            y = Convert.ToDouble(textBox2.Text);
+            z= Convert.ToDouble(textBox3.Text);
+            v= Convert.ToDouble(textBox7.Text);
+            xv = Convert.ToDouble(textBox4.Text);
+            yv = Convert.ToDouble(textBox5.Text);
+            zv = Convert.ToDouble(textBox6.Text);
+            r = Convert.ToDouble(textBox7.Text);
+            newAtom = new Atom(x, y, z, v , xv, yv, zv,r,n);
+
+            currentc.insertAtom(newAtom);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //start
+            timer.Tick += new EventHandler(updateVelocity);
+            timer.Interval = 6000;
+            timer.Start();
+            is_On = true;
+            while (is_On) // timer is on
+            {
+                Application.DoEvents();
+            }
+
+        }
+
+       
+
+        class Atom
+        {
+            public int n;
+            double x, y, z , xv,yv,zv,r;
+            double velocity;
+            public Atom next = null;
+            public Atom() { }
+            public Atom(double x, double y, double z, double velocity , double xv , double yv , double zv, double r,int n)
+            {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+                this.xv = xv;
+                this.yv = yv;
+                this.zv = zv;
+                this.velocity = velocity;
+                this.r = r;
+                this.n = n;
+            }
+            public void setNext(Atom a)
+            { next = a; }
+            public double get_x()
+            { return x; }
+            public int get_n()
+            { return n; }
+            public void set_x(double x1)
+            { x=x1; }
+            public double get_y()
+            { return y; }
+            public void set_y(double y1)
+            { y = y1; }
+            public double get_z()
+            { return z; }
+            public void set_z(double z1)
+            { z = z1; }
+            public double get_vx()
+            { return xv; }
+            public double get_vy()
+            { return yv; }
+            public double get_vz()
+            { return zv; }
+            public double get_r()
+            { return r; }
+            public void set_vx(double xv1)
+            { xv=xv1; }
+            public void set_vy(double yv1)
+            { yv = yv1; }
+            public void set_vz(double zv1)
+            { zv = zv1; }
+
+
+            public double get_yv()
+            { return yv; }
+            public double get_zv()
+            { return zv; }
+            public double get_v()
+            { return velocity; }
+            public void set_v(double v)
+            { velocity = v; }
+            public Atom getNext()
+            { return next; }
+        }
+        class cell
+        {
+            int number;
+            Atom atoms;
+            cell next = null;
+            public cell(int number) { this.number = number ; }
+            public void setAtom(Atom list) { this.atoms = list; }
+            public void setNext(cell c) { next = c; }
+            public int getNum() { return number; }
+            public Atom getAtom() { return atoms; }
+            public cell getNext() { return next; }
+            public void insertAtom(Atom newAtom)
+            {
+                if (atoms == null) atoms = newAtom;
+                else
+                {
+                    Atom train = atoms;
+                    do
+                    {
+
+                        train.setNext(newAtom);
+                        train = train.getNext();
+
+                    } while (train.getNext() != null);
+
+                }
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            if (is_On)
+            {
+                is_On = false;
+                timer.Stop();
+                timer.Tick -= new EventHandler(updateVelocity);
+
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
